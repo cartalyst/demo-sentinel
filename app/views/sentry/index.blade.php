2a818e5817514ea7f30faa813b457786ea404b36
drsii <dan.syme@cartalyst.com>
2014-05-14 00:37:44 -0700

    content.

--- app/views/sentry/index.blade.php
+++ app/views/sentry/index.blade.php
@@ -39,12 +39,8 @@
 <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title" id="myModalLabel">Support</h4>
-      </div>
       <div class="modal-body">
-
+			<p class="lead">Don’t be a mooch! Put in a pull request, help with documentation, or support the cause by subscribing to <a href="https://cartalyst.com">Cartalyst</a>.</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default btn-block" data-dismiss="modal">Close</button>
